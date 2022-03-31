8768220f7a0482630d0a623cc6bc51a175a6fa27
HawDevelopment <70876593+HawDevelopment@users.noreply.github.com>
2022-03-31 16:15:37 +0200

    Refactor

---
+++ src/FileAnalyzer.ts
@@ -0,0 +1,72 @@
+import * as vscode from "vscode";
+import { spawnSync } from "child_process";
+
+export default class FileAnalyzer {
+    document: vscode.TextDocument;
+    collection: vscode.DiagnosticCollection;
+    cwd: string | null;
+    
+    constructor(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, cwd: string | null) {
+        this.document = document;
+        this.collection = collection;
+        this.cwd = cwd;
+    }
+    
+    executeAnalyzer(): string[] {
+        let stdin = this.document.getText();
+        let result = spawnSync("luau-analyze", ["--formatter=plain", "-"], {input: stdin, cwd: this.cwd as any});
+    
+        console.log(result)
+        
+        return result.stdout.toString().split("\n");
+    }
+    
+    createDiagnosticForLine(line: string): vscode.Diagnostic | undefined {
+        let match = line.match(/^(.*):(\d*):(\d*-\d*): \(.*\) (\w*): (.*)/);
+        if (!match || match[1] !== "stdin") {
+            return;
+        }
+        
+        let lineNumber = parseInt(match[2]) - 1;
+        let range = new vscode.Range(
+            new vscode.Position(lineNumber, parseInt(match[3].split("-")[0]) - 1),
+            new vscode.Position(lineNumber, parseInt(match[3].split("-")[1]))
+        )
+        
+        // Find severity
+        let errorName = match[4]
+        let severity = vscode.DiagnosticSeverity.Warning;
+        
+        if (errorName.match(/Error/)) {
+            severity = vscode.DiagnosticSeverity.Error;
+        } else if (errorName.match(/Unused/)) {
+            severity = vscode.DiagnosticSeverity.Information;
+        }
+        
+        // Add error message back to the line
+        let message = `${errorName}: ${match[5]}`;
+        
+        return new vscode.Diagnostic(
+            range,
+            message,
+            severity
+        )
+    }
+    
+    runDiagnostics() {
+        // Clear old diagnostics
+        this.collection.delete(this.document.uri);
+        
+        let errors = this.executeAnalyzer();
+        let newDiagnostics: vscode.Diagnostic[] = [];
+        
+        errors.forEach((line) => {
+            let diagnostic = this.createDiagnosticForLine(line);
+            if (diagnostic) {
+                newDiagnostics.push(diagnostic);
+            }
+        })
+        
+        this.collection.set(this.document.uri, newDiagnostics);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,73 @@
 import * as vscode from "vscode";
-
-let collection: vscode.DiagnosticCollection;
+import FileAnalyzer from "./FileAnalyzer";
 
 function getWorkspacePath(): string | null {
-	let folders = vscode.workspace.workspaceFolders;
-	if (folders) {
-		return folders[0].uri.fsPath;
-	}
-	return null;
+    let folders = vscode.workspace.workspaceFolders;
+    if (!folders) {
+        return null;
+    }
+    return folders[0].uri.fsPath;
 }
+
+class Extension {
+    context: vscode.ExtensionContext;
+    collection: vscode.DiagnosticCollection;
+    fileAnalyzers: Map<string, FileAnalyzer>;
+    
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+        this.fileAnalyzers = new Map<string, FileAnalyzer>();
+        
+        this.collection = vscode.languages.createDiagnosticCollection("luau");
+        this.context.subscriptions.push(this.collection);
+    }
+    
+    deleteFileAnalyzer(document: vscode.TextDocument) {
+        let path = document.uri.fsPath;
+        this.fileAnalyzers.delete(path);
+    }
+    
+    updateDiagnostics(document: vscode.TextDocument): void {
+        let path = document.uri.fsPath;
+        if (document && (path.endsWith(".lua") || path.endsWith(".luau"))) {
+            let analyzer = this.fileAnalyzers.get(path);
+            if (!analyzer) {
+                analyzer = new FileAnalyzer(document, this.collection, getWorkspacePath());
+                this.fileAnalyzers.set(path, analyzer);
+            }
+            analyzer.runDiagnostics();
+        }
+    }
+    
+    activate() {
+        
+        this.context.subscriptions.push(
+            vscode.workspace.onDidOpenTextDocument((document) => {
+                this.updateDiagnostics(document);
+            }),
+            vscode.workspace.onDidChangeTextDocument((event) => {
+                this.updateDiagnostics(event.document);
+            }),
+            vscode.workspace.onDidCloseTextDocument((document) => {
+                this.collection.delete(document.uri);
+                this.deleteFileAnalyzer(document);
+            }),
+        );
+    }
+    
+    deactivate() {
+        this.collection.dispose();
+        this.fileAnalyzers.clear();
+    }
+}
+
+let extension: Extension;
 
 export function activate(context: vscode.ExtensionContext) {
-	collection = vscode.languages.createDiagnosticCollection("luau");
-	context.subscriptions.push(collection);
-
-	if (vscode.window.activeTextEditor) {
-		updateDiagnostics(vscode.window.activeTextEditor.document, collection, getWorkspacePath());
-	}
-
-	context.subscriptions.push(
-		vscode.workspace.onDidOpenTextDocument((document) => {
-			updateDiagnostics(document, collection, getWorkspacePath());
-		})
-	);
-	context.subscriptions.push(
-		vscode.workspace.onDidChangeTextDocument((event) => {
-			updateDiagnostics(event.document, collection, getWorkspacePath());
-		})
-	);
+    extension = new Extension(context)
+    extension.activate();
 }
 
-// Define an execute function
-import {spawnSync} from "child_process";
-function executeIn(stdin: string, cwd: string | null): string {
-	let result = spawnSync("luau-analyze", ["--formatter=plain", "-"], {input: stdin, cwd: cwd as any});
-
-	return result.stdout.toString();
+export function deactivate() {
+    extension.deactivate()
 }
-
-function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection, cwd: string | null): void {
-	let path = document.uri.fsPath;
-
-	if (document && (path.endsWith(".lua") || path.endsWith(".luau"))) {
-		let errors = executeIn(document.getText(), cwd);
-		console.log(errors);
-		let split = errors.split("\n");
-
-		collection.delete(document.uri);
-		let diagnostics: vscode.Diagnostic[] = [];
-
-		split.forEach((line) => {
-			let match = line.match(/^(.*):(\d*):(\d*-\d*): \(.*\) (\w*): (.*)/);
-			if (!match || match[1] !== "stdin") {
-				return;
-			}
-
-			let lineNumber = parseInt(match[2]) - 1;
-			let range1 = parseInt(match[3].split("-")[0]);
-			let range2 = parseInt(match[3].split("-")[1]);
-
-			let severity = vscode.DiagnosticSeverity.Warning;
-
-			if (match[4].match(/Error/)) {
-				severity = vscode.DiagnosticSeverity.Error;
-			} else if (match[4].match(/Unused/)) {
-				severity = vscode.DiagnosticSeverity.Information;
-			}
-
-			let lineDiagnostic = new vscode.Diagnostic(
-				new vscode.Range(new vscode.Position(lineNumber, range1 - 1), new vscode.Position(lineNumber, range2)),
-				match[5] + (match[5].endsWith(".") ? "." : ""),
-				severity
-			);
-
-			diagnostics.push(lineDiagnostic);
-		});
-
-		collection.set(document.uri, diagnostics);
-	} else {
-		collection.clear();
-	}
-}
-
-export function deactivate() {}
